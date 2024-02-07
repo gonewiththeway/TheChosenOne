a0e4dfec4ae41c59db180721226080c49ce273c6
Atul Purohit <gonewiththeway@gmail.com>
2024-02-07 09:27:21 -0500

    minor modification

--- inference.py
+++ inference.py
@@ -22,7 +22,7 @@ pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
 pipe.to("cuda")
 pipe.load_lora_weights(os.path.join(model_path, f"checkpoint-{args.checkpointing_steps * args.num_train_epochs}"))
 
-prompt_postfix = input("Enter the postfix for anime_fox")
+prompt_postfix = input("Enter the postfix for anime_fox:")
 image_postfix = prompt_postfix.replace(" ", "_")
 
 # create folder
@@ -30,6 +30,7 @@ output_folder = f"./inference_results/{args.character_name}"
 # os.makedirs(output_folder)
 
 # remember to use the place holader here
-prompt = f"A photo of {args.placeholder_token}{prompt_postfix}."
+prompt = f"A photo of {args.placeholder_token} {prompt_postfix}."
+print(prompt)
 image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
 image.save(os.path.join(output_folder, f"{args.character_name}_{image_postfix}.png"))
