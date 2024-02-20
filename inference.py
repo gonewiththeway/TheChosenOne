6aff83d2a09ddf87de843da863b6638fbb175913
Atul Purohit <gonewiththeway@gmail.com>
2024-02-19 20:13:38 -0500

    new inference

--- inference.py
+++ inference.py
@@ -16,21 +16,24 @@ def config_2_args(path):
 
 args = config_2_args("config/princess.yaml")
 
-loop = 0
-model_path = os.path.join(args.output_dir, args.character_name, str(loop))
+model_path = os.path.join(args.output_dir, args.character_name)
 pipe = DiffusionPipeline.from_pretrained(model_path, torch_dtype=torch.float16)
 pipe.to("cuda")
 pipe.load_lora_weights(os.path.join(model_path, f"checkpoint-{args.checkpointing_steps * args.num_train_epochs}"))
 
-prompt_postfix = input("Enter the postfix for photo_man:")
-image_postfix = prompt_postfix.replace(" ", "_")
-
-# create folder
 output_folder = f"./inference_results/{args.character_name}"
-# os.makedirs(output_folder)
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
 
-# remember to use the place holader here
-prompt = f"A photo of {args.placeholder_token} {prompt_postfix}."
-print(prompt)
-image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
-image.save(os.path.join(output_folder, f"{args.character_name}_{image_postfix}.png"))
+while True:
+    prompt_postfix = input("Enter the postfix for photo_man or type 'exit' to quit: ")
+    if prompt_postfix.lower() == 'exit':
+        break
+
+    image_postfix = prompt_postfix.replace(" ", "_")
+    prompt = f"A photo of {args.placeholder_token} {prompt_postfix}."
+    print(prompt)
+
+    image = pipe(prompt, num_inference_steps=35, guidance_scale=7.5).images[0]
+    image.save(os.path.join(output_folder, f"{args.character_name}_{image_postfix}.png"))
+    print(f"Image saved as {args.character_name}_{image_postfix}.png in {output_folder}")
